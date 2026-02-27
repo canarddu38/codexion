822dc4bef7e5958578864bf13b5c343ee5e2e273
0x262d <71982379+canarddu38@users.noreply.github.com>
2026-02-27 16:47:00 +0100

    Add files via upload

--- coders/main.c
+++ coders/main.c
@@ -16,20 +16,20 @@
 #include <stdio.h>
 #include <string.h>
 
-int	cleanup(t_args *args, pthread_mutex_t *dongles,
-	t_scheduler *scheduler, t_coder_config *conf)
+int	cleanup(pthread_mutex_t *dongles, t_scheduler *scheduler,
+	t_coder_config *conf, int initialized_coders)
 {
 	int	i;
 
-	if (conf && scheduler)
+	if (conf && scheduler && scheduler->queue)
 		stop_coders(conf);
 	i = 0;
-	while (conf && i < args->nb_coders)
+	while (conf && i < initialized_coders)
 		pthread_mutex_destroy(&conf[i++].time_mutex);
 	if (conf)
 		free(conf);
 	i = 0;
-	while (dongles && i < args->nb_coders)
+	while (dongles && i < initialized_coders)
 		pthread_mutex_destroy(&dongles[i++]);
 	if (dongles)
 		free(dongles);
@@ -37,7 +37,8 @@ int	cleanup(t_args *args, pthread_mutex_t *dongles,
 		return (0);
 	pthread_mutex_destroy(&scheduler->mutex);
 	pthread_cond_destroy(&scheduler->cond);
-	free_lst(scheduler->queue);
+	if (scheduler->queue)
+		free_lst(scheduler->queue);
 	if (scheduler->dongle_state)
 		free(scheduler->dongle_state);
 	if (scheduler->dongle_release)
@@ -49,25 +50,25 @@ static int	init_scheduler(pthread_mutex_t **dongles,
 	t_args *args, t_coder_config **conf,
 	t_scheduler *sch)
 {
+	memset(sch, 0, sizeof(t_scheduler));
 	*dongles = malloc(sizeof(pthread_mutex_t) * args->nb_coders);
 	*conf = malloc(sizeof(t_coder_config) * args->nb_coders);
 	if (!*conf || !*dongles)
-		return (cleanup(args, *dongles, 0, *conf),
+		return (cleanup(*dongles, 0, *conf, 0),
 			puterr("Allocation failed\n"));
 	if (pthread_mutex_init(&sch->mutex, 0) != 0)
-		return (cleanup(args, *dongles, 0, *conf),
+		return (cleanup(*dongles, 0, *conf, 0),
 			puterr("Mutex init failed\n"));
 	if (pthread_cond_init(&sch->cond, 0) != 0)
 	{
 		pthread_mutex_destroy(&sch->mutex);
-		return (cleanup(args, *dongles, 0, *conf),
+		return (cleanup(*dongles, 0, *conf, 0),
 			puterr("Cond init failed\n"));
 	}
-	sch->stop_simulation = 0;
 	sch->dongle_state = malloc(sizeof(int) * args->nb_coders);
 	sch->dongle_release = malloc(sizeof(size_t) * args->nb_coders);
 	if (!sch->dongle_state || !sch->dongle_release)
-		return (cleanup(args, *dongles, sch, *conf),
+		return (cleanup(*dongles, sch, *conf, 0),
 			puterr("Allocation failed\n"));
 	memset(sch->dongle_state, 0, sizeof(int) * args->nb_coders);
 	memset(sch->dongle_release, 0, sizeof(size_t) * args->nb_coders);
@@ -89,11 +90,16 @@ static int	coders_init(t_args *args, pthread_mutex_t *dongles,
 		conf[i].state = COMPILE;
 		conf[i].program_args = *args;
 		conf[i].scheduler_mutex = sch;
-		if (pthread_mutex_init(&conf[i].time_mutex, 0)
-			|| pthread_mutex_init(&dongles[i++], 0))
-			return (1);
+		if (pthread_mutex_init(&conf[i].time_mutex, 0))
+			return (i);
+		if (pthread_mutex_init(&dongles[i], 0))
+		{
+			pthread_mutex_destroy(&conf[i].time_mutex);
+			return (i);
+		}
+		i++;
 	}
-	return (0);
+	return (args->nb_coders);
 }
 
 static int	init_threads(t_coder_config *conf, t_args *args)
@@ -131,6 +137,7 @@ int	main(int argc, char **argv)
 	t_coder_config	*conf;
 	t_scheduler		sch;
 	pthread_mutex_t	*dongles;
+	int				initialized_coders;
 
 	if (argc != 9)
 		return (puterr("Usage: ./codexion <nb_coders> <time_to_burnout> \
@@ -139,12 +146,16 @@ int	main(int argc, char **argv)
 <dongle_cooldown> <scheduler>\n"));
 	if (parse_args(&args, argv) || init_scheduler(&dongles, &args, &conf, &sch))
 		return (1);
-	if (coders_init(&args, dongles, conf, &sch))
-		return (1 + cleanup(&args, dongles, &sch, conf));
+	initialized_coders = coders_init(&args, dongles, conf, &sch);
+	if (initialized_coders != args.nb_coders)
+	{
+		puterr("Could not create coders\n");
+		return (1 + cleanup(dongles, &sch, conf, initialized_coders));
+	}
 	sch.queue = create_lst(args.nb_coders, conf);
 	if (!sch.queue)
-		return (cleanup(&args, dongles, &sch, conf), \
+		return (cleanup(dongles, &sch, conf, initialized_coders), \
 puterr("Cannot allocate dongle release time\n"));
 	init_threads(conf, &args);
-	return (cleanup(&args, dongles, &sch, conf));
+	return (cleanup(dongles, &sch, conf, initialized_coders));
 }
